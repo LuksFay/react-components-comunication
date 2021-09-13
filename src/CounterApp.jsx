5c30310204e8beb97d98b4523a98bfe5eb47cfb6
luksfay <lukspicco@hotmail.com>
2021-09-13 15:08:41 -0300

    counter

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
-const CounterApp = ({value}) => {
+const CounterApp = ({value = 10}) => {
+    const [counter, setCounter] = useState(value);
+   
+   const handleAdd = () => {
+       setCounter(counter + 1);
+   }
+   const handleReset = () => {
+       setCounter(value);
+   }
+   const handleQuit = () => {
+       setCounter(counter - 1);
+   }
+   
     return (
         <>
             <h1>CounterAPP</h1>
             <h2>{value}</h2>
+
+            <button onClick ={handleAdd}>+1</button>
+            <button onClick ={handleReset}>Reset</button>
+            <button onClick ={handleQuit}>-1</button>
         </>
     )
 }
